233eb7f8b5b2d0503dcf8bf6b073596d7ee3708d
Michaël Lebeau <mikaleb@live.fr>
2021-03-17 18:23:19 +0100

    chore(ckeditor): replace ckeditor with my own fork

--- pages/admin/new.vue
+++ pages/admin/new.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="m-6">
+    <input placeholder="title" class="mb-4" />
     <client-only placeholder="loading...">
-      <ckeditor-nuxt v-model="contentHolder" />
+      <ckeditor-nuxt
+        v-model="contentHolder"
+        :config="editorConfig"
+        @ready="onEditorReady"
+      />
       <p>Markdown output :</p>
       <textarea v-html="contentMd"></textarea>
     </client-only>
@@ -17,19 +22,15 @@ export default {
   components: {
     'ckeditor-nuxt': () => {
       if (process.client) {
-        return import('@blowstack/ckeditor-nuxt')
+        return import('@mikaleb/ckeditor-nuxt')
       }
     },
   },
   data() {
     return {
       editorConfig: {
-        simpleUpload: {
-          uploadUrl: 'path_to_image_controller',
-          headers: {
-            Authorization: 'optional_token',
-          },
-        },
+        removePlugins: ['Title'],
+        height: '500px',
       },
       contentHolder: '',
       contentMd: '',
@@ -52,8 +53,21 @@ export default {
   },
 
   methods: {
-    saveContent() {
-      alert(this.content)
+    async saveContent() {
+      console.log(this.contentMd)
+      const { app } = context
+      const defaultLocale = app.i18n.locale
+      const url = `http://localhost:3000/_content/${defaultLocale}`
+      const content = this.contentMd
+      await fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify({ file: content }),
+      }).then((res) => res.json())
+    },
+    onEditorReady(editor) {
+      console.log('🚀 ~ file: new.vue ~ line 69 ~ onReady ~ editor', { editor })
+      // Insert the toolbar before the editable area.
+      editor.config.height = '500'
     },
   },
 }
